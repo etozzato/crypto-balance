517b00434efda6c776e6368e61d26f33202a5299
Emanuele Tozzato <etozzato@gmail.com>
2021-04-26 12:26:34 +0100

    totEur/totUsd

--- balance.go
+++ balance.go
@@ -13,9 +13,11 @@ import (
 )
 
 type conf struct {
-	Xmr  float32 `yaml:"xmr"`
-	Eth  float32 `yaml:"eth"`
-	Doge float32 `yaml:"doge"`
+	Xmr      float32 `yaml:"xmr"`
+	Eth      float32 `yaml:"eth"`
+	Doge     float32 `yaml:"doge"`
+	Xtz      float32 `yaml:"xtz"`
+	Nucypher float32 `yaml:"nucypher"`
 }
 
 func (balance *conf) getConf() *conf {
@@ -38,7 +40,7 @@ func main() {
 	p := message.NewPrinter(message.MatchLanguage("en"))
 
 	cg := coingecko.NewClient(nil)
-	coins := []string{"monero", "ethereum", "dogecoin"}
+	coins := []string{"monero", "ethereum", "dogecoin", "tezos", "nucypher"}
 	fiats := []string{"usd", "eur"}
 	dt := time.Now()
 	prices, err := cg.SimplePrice(coins, fiats)
@@ -48,6 +50,11 @@ func main() {
 	xmr := (*prices)["monero"]
 	eth := (*prices)["ethereum"]
 	doge := (*prices)["dogecoin"]
+	tezos := (*prices)["tezos"]
+	nucypher := (*prices)["nucypher"]
+
+	totEur := xmr["eur"]*balance.Xmr + eth["eur"]*balance.Eth + doge["eur"]*balance.Doge + tezos["eur"]*balance.Xtz + nucypher["eur"]*balance.Nucypher
+	totUsd := xmr["usd"]*balance.Xmr + eth["usd"]*balance.Eth + doge["usd"]*balance.Doge + tezos["usd"]*balance.Xtz + nucypher["usd"]*balance.Nucypher
 
 	data := [][]string{
 		[]string{
@@ -74,6 +81,22 @@ func main() {
 			p.Sprintf("%.4f", doge["eur"]),
 			p.Sprintf("%.2f", doge["eur"]*balance.Doge),
 		},
+		[]string{
+			"XTZ",
+			p.Sprintf("%f", balance.Xtz),
+			p.Sprintf("%.4f", tezos["usd"]),
+			p.Sprintf("%.2f", tezos["usd"]*balance.Xtz),
+			p.Sprintf("%.4f", tezos["eur"]),
+			p.Sprintf("%.2f", tezos["eur"]*balance.Xtz),
+		},
+		[]string{
+			"NuCy",
+			p.Sprintf("%f", balance.Nucypher),
+			p.Sprintf("%.4f", nucypher["usd"]),
+			p.Sprintf("%.2f", nucypher["usd"]*balance.Nucypher),
+			p.Sprintf("%.4f", nucypher["eur"]),
+			p.Sprintf("%.2f", nucypher["eur"]*balance.Nucypher),
+		},
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
@@ -89,9 +112,9 @@ func main() {
 		"🗓️",
 		dt.Local().Format("2 Jan 2006 15:04"),
 		"Total 💵",
-		p.Sprintf("%.2f", xmr["usd"]*balance.Xmr+eth["usd"]*balance.Eth+doge["usd"]*balance.Doge),
+		p.Sprintf("%.2f", totUsd),
 		"Total 💶",
-		p.Sprintf("%.2f", xmr["eur"]*balance.Xmr+eth["eur"]*balance.Eth+doge["eur"]*balance.Doge),
+		p.Sprintf("%.2f", totEur),
 	})
 	table.SetBorder(true)
 	table.AppendBulk(data)
